8120e06994741d2ae7369c49140a9105bfd7a74b
MANSI BAJAJ <bmansi362@gmail.com>
2023-01-13 20:01:01 +0530

    message

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,23 @@
 package com.driver;
 
 public class Main {
-  
+
+
+    public static void main( String[]arg){
+        B b=new B();
+        System.out.println(b.meth());
+
+       // System.out.println("This is asample solution");
+    }
+    public static class A{
+     public static String meth(){
+         return "Invoking method from class A";
+
+        }
+    }
+    public static class B extends A{
+        public static String meth(){
+            return"Method is overridden in Extended class B";
+        }
+    }
 }
